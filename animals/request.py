f185761a3c499ab0882e9ed1f05183fc99b5c164
DArmstrong87 <lenzflare@gmail.com>
2021-10-11 15:43:28 -0500

    using params to get single items as needed

--- animals/request.py
+++ animals/request.py
@@ -1,27 +1,83 @@
 ANIMALS = [
     {
-        "id": 1,
-        "name": "Snickers",
-        "species": "Dog",
-        "locationId": 1,
-        "customerId": 4
+      "id": 1,
+      "name": "Doodles",
+      "breed": "German Shepherd",
+      "locationId": 1
     },
     {
-        "id": 2,
-        "name": "Gypsy",
-        "species": "Dog",
-        "locationId": 1,
-        "customerId": 2
+      "id": 3,
+      "name": "Angus",
+      "breed": "Dalmatian 👾",
+      "locationId": 1
     },
     {
-        "id": 3,
-        "name": "Blue",
-        "species": "Cat",
-        "locationId": 2,
-        "customerId": 1
+      "id": 4,
+      "name": "Henley",
+      "breed": "Carolina Retriever 🚒",
+      "locationId": 1
+    },
+    {
+      "id": 5,
+      "name": "Derkins",
+      "breed": "Shih tzu 👿",
+      "locationId": 2
+    },
+    {
+      "id": 6,
+      "name": "Checkers",
+      "breed": "Bulldog",
+      "locationId": 1
+    },
+    {
+      "name": "Sawyer",
+      "breed": "Lollie",
+      "id": 7,
+      "locationId": 2
+    },
+    {
+      "name": "Gypsy",
+      "breed": "Miniature Schnauzer",
+      "id": 8,
+      "locationId": 1
+    },
+    {
+      "name": "Zipper",
+      "breed": "Terrier",
+      "locationId": 2,
+      "id": 9
+    },
+    {
+      "name": "Blue",
+      "breed": "Hound dog",
+      "locationId": 2,
+      "id": 10
+    },
+    {
+      "name": "JOE",
+      "breed": "Husky",
+      "locationId": 2,
+      "id": 11
     }
 ]
 
 
 def get_all_animals():
+    '''Get all animals'''
     return ANIMALS
+# Function with a single parameter
+
+
+def get_single_animal(id):
+    # Variable to hold the found animal, if it exists
+    requested_animal = None
+
+    # Iterate the ANIMALS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for animal in ANIMALS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if animal["id"] == id:
+            requested_animal = animal
+
+    return requested_animal
--- employees/request.py
+++ employees/request.py
@@ -11,11 +11,27 @@ EMPLOYEES = [
     },
     {
         "id": 3,
-        "name": "Nikolas",
+        "name": "Nick",
         "locationId": 2
     }
 ]
 
 
 def get_all_employees():
+    '''Returns all employees'''
     return EMPLOYEES
+
+
+def get_single_employee(id):
+    # Variable to hold the found employee, if it exists
+    requested_employee = None
+
+    # Iterate the EMPLOYEE list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for employee in EMPLOYEES:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if employee["id"] == id:
+            requested_employee = employee
+
+    return requested_employee
