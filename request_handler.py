0ed9a9476dad4f67259a06526ec0e16b408d2cde
DArmstrong87 <lenzflare@gmail.com>
2021-10-11 11:56:12 -0500

    modified request handler

--- request_handler.py
+++ request_handler.py
@@ -5,6 +5,8 @@ from animals import get_all_animals
 # For now, think of a class as a container for functions that
 # work together for a common purpose. In this case, that
 # common purpose is to respond to HTTP requests from a client.
+
+
 class HandleRequests(BaseHTTPRequestHandler):
     # This is a Docstring it should be at the beginning of all classes and functions
     # It gives a description of the class or function
@@ -31,8 +33,10 @@ class HandleRequests(BaseHTTPRequestHandler):
         """
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.send_header('Access-Control-Allow-Methods',
+                         'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers',
+                         'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
     # Here's a method on the class that overrides the parent's method.
@@ -52,7 +56,6 @@ class HandleRequests(BaseHTTPRequestHandler):
         else:
             response = []
 
-
         # This weird code sends a response back to the client
         self.wfile.write(f"{response}".encode())
 
@@ -66,7 +69,7 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
+        response = f"received post request: {post_body}"
         self.wfile.write(response.encode())
 
     # Here's a method on the class that overrides the parent's method.
@@ -90,4 +93,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
