b3b7789ecf476e57a1cb2d9b87722efb3f487aa4
DArmstrong87 <lenzflare@gmail.com>
2021-10-12 16:02:22 -0500

    created classes for animals, customers, employees and locations

--- request_handler.py
+++ request_handler.py
@@ -4,6 +4,7 @@ from locations import get_all_locations, get_single_location, create_location, d
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
+from models import Animal, Customer, Employee, Location
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
