68b1b4923b8777b2152c5b129a8ecd5d66d762dc
DArmstrong87 <lenzflare@gmail.com>
2021-10-13 16:14:52 -0500

    integrating sql

--- models/customer.py
+++ models/customer.py
@@ -3,8 +3,9 @@ class Customer():
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, id, name, email, employee):
+    def __init__(self, id, name, email, address, password):
         self.id = id
         self.name = name
+        self.address = address
         self.email = email
-        self.employee = employee
+        self.password = password
--- request_handler.py
+++ request_handler.py
@@ -4,7 +4,7 @@ from locations import get_all_locations, get_single_location, create_location, d
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
-from models import Animal, Customer, Employee, Location
+# from models import Animal, Customer, Employee, Location
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -44,8 +44,30 @@ class HandleRequests(BaseHTTPRequestHandler):
                          'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
+    def parse_url(self, path):
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        # Try to get the item at index 2
+        try:
+            # Convert the string "1" to the integer 1
+            # This is the new parseInt()
+            id = int(path_params[2])
+        except IndexError:
+            pass  # No route parameter exists: /animals
+        except ValueError:
+            pass  # Request had trailing slash: /animals/
+
+        return (resource, id)  # This is a tuple
+
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
+
     def do_GET(self):
         """Getter function"""
         self._set_headers(200)
@@ -92,27 +114,21 @@ class HandleRequests(BaseHTTPRequestHandler):
         (resource, id) = self.parse_url(self.path)
 
         # Initialize new...
-        new_animal = None
-        new_location = None
-        new_employee = None
-        new_customer = None
+        new_response = None
 
         # Add a new animal to the list. Don't worry about
         # the orange squiggle, you'll define the create_animal
         # function next.
         if resource == "animals":
-            new_animal = create_animal(post_body)
-            self.wfile.write(f"{new_animal}".encode())
+            new_response = create_animal(post_body)
         elif resource == "locations":
-            new_location = create_location(post_body)
-            self.wfile.write(f"{new_location}".encode())
+            new_response = create_location(post_body)
         elif resource == "employees":
-            new_employee = create_employee(post_body)
-            self.wfile.write(f"{new_employee}".encode())
+            new_response = create_employee(post_body)
         elif resource == "customers":
-            new_customer = create_customer(post_body)
-            self.wfile.write(f"{new_customer}".encode())
+            new_response = create_customer(post_body)
 
+        self.wfile.write(f"{new_response}".encode())
         # Encode the new dict and send in response
 
     # Here's a method on the class that overrides the parent's method.
@@ -140,32 +156,11 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Encode the new animal and send in response
         self.wfile.write("".encode())
 
-    def parse_url(self, path):
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
-        path_params = path.split("/")
-        resource = path_params[1]
-        id = None
-
-        # Try to get the item at index 2
-        try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
-            pass  # No route parameter exists: /animals
-        except ValueError:
-            pass  # Request had trailing slash: /animals/
-
-        return (resource, id)  # This is a tuple
-
     def do_DELETE(self):
         # Set a 204 response code
         self._set_headers(204)
 
-        # Parse the URL
+        # Parse the URL or unpack the tuple
         (resource, id) = self.parse_url(self.path)
 
         # Delete a single animal from the list
